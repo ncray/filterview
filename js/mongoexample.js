41d71e6584c609a1c5c22d7780841fca83dc9916
Jackson Gorham <jacksongorham@gmail.com>
2011-02-09 15:23:03 -0800

    Added MongoDB walkthrough.

---
+++ js/mongoexample.js
@@ -0,0 +1,2 @@
+require(["order!thirdparty/js/jquery-1.4.4.min.js",
+         "order!js/toc.js"]);
--- js/toc.js
+++ js/toc.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // The html is dynamically added on page load
-    var toc_html = '<div id="toc"><h2><a href="index.html">FilterView Docs</a></h2><ul><li class="chapter"><a href="syntax.html">Syntax</a></li><li class="chapter menu"><a href="#">Examples with Data</a><ul class="dropdown"><li><a href="ajaxexample.html">Locally Stored</a></li><li><a href="jsonpexample.html">Via JSONP</a></li></ul></li><li class="chapter">Source</li></ul></div>';
+    var toc_html = '<div id="toc"><h2><a href="index.html">FilterView Docs</a></h2><ul><li class="chapter"><a href="syntax.html">Syntax</a></li><li class="chapter menu"><a href="#">Examples with Data</a><ul class="dropdown"><li><a href="ajaxexample.html">Locally Stored</a></li><li><a href="jsonpexample.html">Via JSONP</a></li><li><a href="mongoexample.html">Using Node.js and MongoDB</a></li></ul></li><li class="chapter">Source</li></ul></div>';
     $(toc_html).prependTo("body");
 
     // Code that enables toc navigation
