6737dce445f973c60cbe314851645e881bf88faf
Jackson Gorham <jacksongorham@gmail.com>
2011-02-09 11:43:34 -0800

    Now add the toc navigation dynamically.
    This was done to create a single point of truth, since this is on every
    page besides the source code.

--- js/toc.js
+++ js/toc.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    // The html is dynamically added on page load
+    var toc_html = '<div id="toc"><h2><a href="index.html">FilterView Docs</a></h2><ul><li class="chapter"><a href="syntax.html">Syntax</a></li><li class="chapter menu"><a href="#">Examples with Data</a><ul class="dropdown"><li><a href="ajaxexample.html">Locally Stored</a></li><li><a href="jsonpexample.html">Via JSONP</a></li></ul></li><li class="chapter">Source</li></ul></div>';
+    $(toc_html).prependTo("body");
+
     // Code that enables toc navigation
     var dropClicked = false;
     $(".menu").click(function(e) {
