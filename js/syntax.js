f32df61d87f7b4386fcd612d60a9bdb0b21209f5
Jackson Gorham <jacksongorham@gmail.com>
2011-02-09 14:49:24 -0800

    Added postFn example to syntax.html page.

--- js/syntax.js
+++ js/syntax.js
@@ -3,6 +3,36 @@ require(["order!thirdparty/js/jquery-1.4.4.min.js",
          "order!data/clusters.js",
          "order!jscliplot"], function (_, _, foo, jscliplot) {
              var exmpl = '<div id="${contid}" class="right_half"><div id="${svgid}" /><div id="${uiid}"/></div>';
+
+             // Definition of post function for postFn example
+             var drawCLs = function (datapts) {
+                 if (!datapts.length) return;
+                 var yys = datapts.map(function(pt) {return pt.yy});
+                 var ybar = (yys.reduce(function(prev, curr) {
+                     return prev+curr;
+                 })/(yys.length));
+                 var std = Math.pow(yys.map(function(y) {return Math.pow(y-ybar, 2)}).reduce(function(prev, curr) {
+                     return prev+curr;
+                 })/(yys.length - 1), 0.5);
+                 var xmin = this.xAxis._scale.min,
+                 xmax = this.xAxis._scale.max,
+                 lcl = ybar - 2*std,
+                 ucl = ybar + 2*std;
+
+                 [lcl, ucl].forEach(function(y) {
+                     var ptone = this._getSVGCoords(xmin, y);
+                     var pttwo = this._getSVGCoords(xmax, y);
+                     this._wrapper.line(this._plotCont, ptone[0], ptone[1], pttwo[0], pttwo[1], {stroke: "red", strokeWidth: 2});
+                 }, this);
+                 // Now highlight the ones outside the bands
+                 var svgpts = this._datapointCont.childNodes;
+                 for (var i = 0; i < svgpts.length; i++) {
+                     var yco = this._getPlotCoords(null, this._getValue(svgpts[i],"cy"));
+                     if ((yco > ucl) || (yco < lcl)) {
+                         svgpts[i].setAttribute("fill", "orange");
+                     }
+                 }
+             };
              var fncalls = [
                  [foo.xx],
                  [foo.xx, foo.yy],
@@ -11,7 +41,8 @@ require(["order!thirdparty/js/jquery-1.4.4.min.js",
                  [foo.xx, foo.yy, {col:foo.col, tt:foo.tt, ui:{checkbox:"tt", autocomplete:"col"}}],
                  [foo.xx, foo.yy, {col:foo.col, ui:{slider:["xx","yy"]}}],
                  [foo.xx, foo.yy, {col:foo.col, rescale:true, ui:{regexp:"col"}}],
-                 [foo.xx, foo.yy, {col:foo.col, ui:{slider:"xx"}, cui:{checkbox:"col"}}]
+                 [foo.xx, foo.yy, {col:foo.col, ui:{slider:"xx"}, cui:{checkbox:"col"}}],
+                 [foo.yy, {col:foo.col, type:"o", postFns:[drawCLs], ui: {checkbox: "col"}}]
              ];
              $(".show").click(function() {
                  var wasEmpty = ($(this).text() == "Show");
