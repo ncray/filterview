792fb90cc4cfc5bcea1d6b7c0d357a0f4f4c7c4a
Jackson Gorham <jacksongorham@gmail.com>
2011-02-10 13:35:11 -0800

    Fixed grid issue and postFn lines now run behind points.

--- js/test.js
+++ js/test.js
@@ -7,18 +7,18 @@
 require(["order!thirdparty/js/jquery-1.4.4.min.js",
          "order!jscliplot"],
          function (_, jscliplot) {
-                // $.get("data/three_cluster.json", function (foo) {
-                //     var fncalls = {
-                //         'plot(foo.xx)': [foo.xx],
-                //         'plot(foo.xx, foo.yy)': [foo.xx, foo.yy],
-                //         'plot(foo.xx, foo.yy, {col: foo.col})': [foo.xx, foo.yy, {col: foo.col}],
-                //         'plot(foo.xx, foo.yy, {col: foo.col, ui:{checkbox:"col"}})' : [foo.xx, foo.yy, {col:foo.col, ui:{checkbox:"col"}}],
-                //         'plot(foo.xx, foo.yy, {col: foo.col, tt:foo.tt, ui:{slider:["tt", "yy"]}})' : [foo.xx, foo.yy, {col: foo.col, tt:foo.tt, ui:{slider:["tt","yy"]}}],
-                //         'plot(foo.xx, foo.yy, {col: foo.col, ui:{dropdown: "col"}})' : [foo.xx, foo.yy, {col: foo.col, ui:{dropdown: "col"}}],
-                //         'plot(foo.xx, foo.yy, {col: foo.col, ui:{autocomplete: "col"}})' : [foo.xx, foo.yy, {col: foo.col, ui:{autocomplete: "col"}}],
-                //         'plot(foo.xx, foo.yy, {col: foo.col, ui:{regexp: "col"}})' : [foo.xx, foo.yy, {col: foo.col, ui:{regexp: "col"}}],
-                //         'plot(foo.xx, foo.yy, {col:foo.col, tt:foo.tt, ui:{checkbox:"col",slider:"tt"}})' : [foo.xx, foo.yy, {col:foo.col, tt:foo.tt, ui:{checkbox:"col",slider:"tt"}}],
-                //     };
+                //$.get("data/three_cluster.json", function (foo) {
+                    // var fncalls = {
+                        // 'hist(foo.xx)': [foo.xx],
+                        // 'plot(foo.xx, foo.yy)': [foo.xx, foo.yy],
+                        // 'plot(foo.xx, foo.yy, {col: foo.col})': [foo.xx, foo.yy, {col: foo.col}],
+                        // 'plot(foo.xx, foo.yy, {col: foo.col, ui:{checkbox:"col"}})' : [foo.xx, foo.yy, {col:foo.col, ui:{checkbox:"col"}}],
+                        // 'plot(foo.xx, foo.yy, {col: foo.col, tt:foo.tt, ui:{slider:["tt", "yy"]}})' : [foo.xx, foo.yy, {col: foo.col, tt:foo.tt, ui:{slider:["tt","yy"]}}],
+                        // 'plot(foo.xx, foo.yy, {col: foo.col, ui:{dropdown: "col"}})' : [foo.xx, foo.yy, {col: foo.col, ui:{dropdown: "col"}}],
+                        // 'plot(foo.xx, foo.yy, {col: foo.col, ui:{autocomplete: "col"}})' : [foo.xx, foo.yy, {col: foo.col, ui:{autocomplete: "col"}}],
+                        // 'plot(foo.xx, foo.yy, {col: foo.col, ui:{regexp: "col"}})' : [foo.xx, foo.yy, {col: foo.col, ui:{regexp: "col"}}],
+                        // 'plot(foo.xx, foo.yy, {col:foo.col, tt:foo.tt, ui:{checkbox:"col",slider:"tt"}})' : [foo.xx, foo.yy, {col:foo.col, tt:foo.tt, ui:{checkbox:"col",slider:"tt"}}],
+                    // };
              // $.get("data/fake_genome.json", function (foo) {
              //     var fncalls = {
              //         'plot(foo.pos, foo.coverage, {gene: foo.gene, exon: foo.exon, ui:{slider: "exon", autocomplete: "gene"}})' : [foo.pos, foo.coverage, {gene: foo.gene, exon: foo.exon, ui:{autocomplete: "gene", slider: "exon"}}],
@@ -49,14 +49,14 @@ require(["order!thirdparty/js/jquery-1.4.4.min.js",
                      return prev+curr;
                  })/(yys.length - 1), 0.5);
                  var xmin = this.xAxis._scale.min,
-                     xmax = this.xAxis._scale.max,
-                     lcl = xbar - 2*std,
-                     ucl = xbar + 2*std;
+                 xmax = this.xAxis._scale.max,
+                 lcl = xbar - 2*std,
+                 ucl = xbar + 2*std;
 
                  [lcl, ucl].forEach(function(y) {
                      var ptone = this._getSVGCoords(xmin, y);
                      var pttwo = this._getSVGCoords(xmax, y);
-                     this._wrapper.line(this._plotCont, ptone[0], ptone[1], pttwo[0], pttwo[1], {stroke: "red", strokeWidth: 2});
+                     this._wrapper.line(this._bg, ptone[0], ptone[1], pttwo[0], pttwo[1], {stroke: "red", strokeWidth: 2});
                  }, this);
                  // Now highlight the ones outside the bands
                  var svgpts = this._datapointCont.childNodes;
@@ -82,24 +82,24 @@ require(["order!thirdparty/js/jquery-1.4.4.min.js",
                  'plot(foo.yy, {col: foo.col, type:"o", postFns:[drawCLs], ui:{checkbox:"col"}})' : [foo.yy, {col:foo.col, type:"o", postFns: [drawCLs], ui:{checkbox:"col"}}],
              };
 
-                    var svgtmpl = '<div id="${id}" class="right_half"></div>';
-                    var uitmpl  = '<div id="${id}" class="left_half"></div>';
-                    var uihead = '<div class="command"><h2>#${i} Command: ${cmd}</h2></div>';
-                    var cont = ".container";
-                    var i = 1;
+             var svgtmpl = '<div id="${id}" class="right_half"></div>';
+             var uitmpl  = '<div id="${id}" class="left_half"></div>';
+             var uihead = '<div class="command"><h2>#${i} Command: ${cmd}</h2></div>';
+             var cont = ".container";
+             var i = 1;
 
-                    for (var cmd in fncalls) {
-                        var pslave = "#plotslave"+i;
-                        var uislave = "#uislave"+i;
-                        $.tmpl(uitmpl, {id:uislave.substr(1)}).appendTo(cont);
-                        $.tmpl(svgtmpl, {id:pslave.substr(1)}).appendTo(cont);
-                        $.tmpl(uihead, {cmd: cmd, i:i}).appendTo(uislave);
-                        $(pslave).css('height', $(pslave).css('width'));
+             for (var cmd in fncalls) {
+                 var pslave = "#plotslave"+i;
+                 var uislave = "#uislave"+i;
+                 $.tmpl(uitmpl, {id:uislave.substr(1)}).appendTo(cont);
+                 $.tmpl(svgtmpl, {id:pslave.substr(1)}).appendTo(cont);
+                 $.tmpl(uihead, {cmd: cmd, i:i}).appendTo(uislave);
+                 $(pslave).css('height', $(pslave).css('width'));
 
-                        jscliplot.createSVGPlot(pslave, uislave);
-                        jscliplot.plot.apply(jscliplot, fncalls[cmd]);
-                        $('<div class="clear"></div>').appendTo(cont);
-                        i++;
-                    }
-//                });
+                 jscliplot.createSVGPlot(pslave, uislave);
+                 jscliplot.plot.apply(jscliplot, fncalls[cmd]);
+                 $('<div class="clear"></div>').appendTo(cont);
+                 i++;
+             }
+             //                });
          });
